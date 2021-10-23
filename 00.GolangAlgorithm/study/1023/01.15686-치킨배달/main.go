361444a5379a4a0a243c2075469840a43fbc13dc
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-10-23 23:54:00 +0900

    no message

--- 00.GolangAlgorithm/study/1023/01.15686-치킨배달/main.go
+++ 00.GolangAlgorithm/study/1023/01.15686-치킨배달/main.go
@@ -6,55 +6,42 @@ import (
 	"os"
 )
 
-var N, M, answer int
-
-var board [51][51]int
-
 type Data struct {
-	y int
-	x int
+	y, x int
 }
 
-var chicken []Data // 치킨 데이터 저장하는 슬라이스
-var D []int        //치킨집 뽑기
-func init1() {
-	N = 0
-	M = 0
-	answer = 0x7fffffff
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Fscanln(reader, &N, &M)
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			fmt.Fscanln(reader, &board[i][j])
-			if board[i][j] == 2 {
-				chicken = append(chicken, Data{i, j})
-			}
+var (
+	n, m, answer int
+	board        [51][51]int
+	reader       *bufio.Reader = bufio.NewReader(os.Stdin)
+	writer       *bufio.Writer = bufio.NewWriter(os.Stdout)
+	chicken      []Data
+)
+
+func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
+func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
+
+func inits() {
+	n = 0
+	m = 0
+	answer = 0
+	scanf("%d %d\n", &n, &m)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			scanf("%d", &board[i][j])
 		}
 	}
 }
+func dfs(int idx, int cnt) {
 
-// func dfs(idx, cnt int) {
-// 	if idx >= len(chicken) {
-// 		return
-// 	}
-// 	if cnt == M-1 {
-// 	}
-// 	D = append(D, idx)
-// 	dfs(idx+1, cnt+1)
-// 	D = D[:len(D)-1]
-
-// }
-
+}
 func main() {
+	defer writer.Flush()
 	T := 0
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	fmt.Fscanln(reader, &T)
+	scanf("%d", &T)
 	for testCase := 1; testCase <= T; testCase++ {
-		init1()
-		fmt.Fprintln(writer, chicken)
-		//dfs(0, 0)
-		fmt.Fprintln(writer, testCase, answer)
+		inits()
+		dfs(0, 0)
+		printf("#%d %d\n", testCase, answer)
 	}
-	writer.Flush()
 }

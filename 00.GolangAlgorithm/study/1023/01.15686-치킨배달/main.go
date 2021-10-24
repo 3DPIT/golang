da4a915c9f5804fc1c856a77270fced6f4a33048
DESKTOP-7SGCC5K\oksos <oksos0817@naver.com>
2021-10-24 17:04:27 +0900

    [21.10.24] docs: golang 15686-치킨배달 풀이
    처음 고랭으로 알고리즘을 하려고 하니 맘처럼 쉽지 않음 그래서 최대한 익숙해지지기 위해 노력 해야함

--- 00.GolangAlgorithm/study/1023/01.15686-치킨배달/main.go
+++ 00.GolangAlgorithm/study/1023/01.15686-치킨배달/main.go
@@ -11,37 +11,95 @@ type Data struct {
 }
 
 var (
-	n, m, answer int
-	board        [51][51]int
-	reader       *bufio.Reader = bufio.NewReader(os.Stdin)
-	writer       *bufio.Writer = bufio.NewWriter(os.Stdout)
-	chicken      []Data
+	n, m    int
+	answer                = 0
+	board                 = [51][51]int{}
+	reader  *bufio.Reader = bufio.NewReader(os.Stdin)
+	writer  *bufio.Writer = bufio.NewWriter(os.Stdout)
+	chicken []Data
+	home    []Data
+	D       [15]int
 )
 
 func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
 func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
 
+func main() {
+	defer writer.Flush()
+	inits()
+	dfs(0, 0)
+	printf("%d\n", answer)
+}
 func inits() {
+
 	n = 0
 	m = 0
-	answer = 0
+	answer = 0x7fffffff
 	scanf("%d %d\n", &n, &m)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			scanf("%d", &board[i][j])
+			fmt.Fscan(reader, &board[i][j])
+			if 1 == board[i][j] {
+				home = append(home, Data{i, j})
+			}
+			if 2 == board[i][j] {
+				chicken = append(chicken, Data{i, j})
+			}
+
 		}
 	}
+	// fmt.Println(chicken)
+	// fmt.Println(home)
 }
-func dfs(int idx, int cnt) {
 
-}
-func main() {
-	defer writer.Flush()
-	T := 0
-	scanf("%d", &T)
-	for testCase := 1; testCase <= T; testCase++ {
-		inits()
-		dfs(0, 0)
-		printf("#%d %d\n", testCase, answer)
+func abs(num int) int {
+	if num < 0 {
+		num = num * -1
 	}
+	return num
+}
+
+func dfs(idx, cnt int) {
+
+	if idx > len(chicken) {
+		return
+	}
+	if cnt == m {
+		// fmt.Println(D)
+		sum := 0
+		// fmt.Println("homeLen", len(home))
+		// fmt.Println("chichken", len(chicken))
+		homeMin := 0
+		for i := 0; i < len(home); i++ {
+			homeMin = 0x7fffffff
+			for j := 0; j < len(chicken); j++ {
+				// fmt.Println(i, j)
+				if D[j] == 1 {
+					absNum := abs(home[i].y-chicken[j].y) + abs(home[i].x-chicken[j].x)
+					// fmt.Println("y", home[i].y, chicken[j].y)
+					// fmt.Println("x", home[i].x, chicken[j].x)
+					// fmt.Println("absNum", absNum)
+					if homeMin > absNum {
+						homeMin = absNum
+						// fmt.Println("homeMin", homeMin)
+					}
+				}
+			}
+			sum += homeMin
+
+			// fmt.Println("homMin fix", homeMin)
+		}
+		// fmt.Println("sum", sum)
+		// fmt.Println("answer", answer)
+
+		if answer > sum {
+			answer = sum
+		}
+		return
+	}
+
+	D[idx] = 1
+	dfs(idx+1, cnt+1)
+	D[idx] = 0
+	dfs(idx+1, cnt)
 }
